3d3f2a5befbd910294a7b1c5c1fa56e6747e79ed
gmcd025 <43909008+gmcd025@users.noreply.github.com>
2019-03-24 18:45:16 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 console.log("Welcome to my website and the dev tools!");
 //console.log("Press enter to find out more information");
 
+let name = '';
+
+console.log("Welcome to my website and the dev tools!");
+
+console.log("Please enter 'userName()'");
+
+
+function userName() {
+  name = prompt("Please enter your name");
+  console.log("Hi " + name); 
+  console.log("Please enter 'banner()'");
+  return name;
+} 
+
+function banner() {
+  document.querySelector("h1").innerText = name;
+  console.log("Please enter wordGame();")
+}
+
+function wordGame() {
+  let words = document.querySelector(".header_intro");
+  noun = prompt("Please enter a noun");
+  verb = prompt("Please enter a verb");
+  adj = prompt("Please enter an adjective");
+  words.innerText ="Hi, I'm " + name + ", I'm a Front End Developer and I " + verb + " to make " + adj + " " + noun;
+}
